03bef542a6eeb64cdcbb4e7f09da21ea7405bd7d
Erturgor <96728263+Erturgor@users.noreply.github.com>
2025-01-10 23:25:37 +0100

    raporty

--- HeurystykaAPI/BestParametersController.cs
+++ HeurystykaAPI/BestParametersController.cs
@@ -1,20 +1,21 @@
 ﻿using Heurystyka.Domain;
 using Heurystyka.Infrastructure;
+using iText.Commons.Actions.Contexts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Runtime.CompilerServices;
 
 namespace HeurystykaAPI
 {
-    public class BestParametersController : BaseApiController
+    public class DatabaseController : BaseApiController
     {
         private readonly DataContext dataContext;
-        public BestParametersController(DataContext dataContext)
+        public DatabaseController(DataContext dataContext)
         {
             this.dataContext = dataContext;
         }
 
-        [HttpGet]
+        [HttpGet("Parametry")]
         public async Task<ActionResult<AlgorithmResult>> GetParameters(string name)
         {
             var result = await dataContext.AlgorithmResults.Include(ar => ar.Parameters).FirstOrDefaultAsync(ar => ar.AlgorithmName == name);
@@ -23,6 +24,21 @@ namespace HeurystykaAPI
                 return NotFound("Algorithm result not found.");
             }
             return Ok(result);
+        }
+        [HttpGet("Raporty")]
+        public async Task<IActionResult> GetAllAlgorithmResults()
+        {
+            var algorithmResults = await dataContext.AlgorithmResults
+                .Include(ar => ar.Parameters)  
+                .ToListAsync(); 
+
+      
+            if (algorithmResults == null || !algorithmResults.Any())
+            {
+                return NoContent(); 
+            }
+
+            return Ok(algorithmResults);
         }
 
         [HttpPost]
